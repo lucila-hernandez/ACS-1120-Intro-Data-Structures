fb02779ce3b80c419d1d1461d6aa083cc7c3f804
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-02-05 15:26:31 -0800

    updated listogram.py

--- Code/listogram.py
+++ Code/listogram.py
@@ -20,26 +20,55 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        found = False
+        for i, (existing_word, freq) in enumerate(self): 
+            if existing_word == word: 
+                self[i] = (existing_word, freq + count)
+                found = True
+                break
+        
+        if not found: 
+            self.append((word, count))
+            self.types +=1 
+
+        self.tokens += count
+
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        for existing_word, freq in self:
+            if existing_word == word:
+                return freq
+        return 0 
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
-        # TODO: Check if word is in this histogram
+        for existing_word, _ in self:
+            if existing_word == word:
+                return True
+        return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
-        # TODO: Implement linear search to find index of entry with target word
+        for index, (word, _) in enumerate(self):
+            if word == target:
+                return index
+        return None 
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
-        # TODO: Randomly choose a word based on its frequency in this histogram
+        if self.tokens == 0:
+            return None
+        
+        random_index = random.randint(1, self.tokens)
 
+        cumulative_count = 0
+        for word, count in self:
+            cumulative_count += count
+            if cumulative_count >= random_index:
+                return word
 
 def print_histogram(word_list):
     print()
